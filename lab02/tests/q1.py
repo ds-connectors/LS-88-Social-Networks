4424566e89b212bff689c09db2e240f2c09b66f9
dennis feehan <feehan@berkeley.edu>
2017-09-18 10:59:10 -0700

    lab02

---
+++ lab02/tests/q1.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> x
+          9182017
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
